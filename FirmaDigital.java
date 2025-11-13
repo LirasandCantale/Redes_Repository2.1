7a16aefbf83c86156f6208f3f0b4e6c792614258
LirasandCantale <benjapuratich@gmail.com>
2025-11-13 19:48:46 -0300

    Change signature encoding from Base64 to HEX
    Updated signature methods to use HEX encoding instead of Base64.

--- FirmaDigital.java
+++ FirmaDigital.java
@@ -1,19 +1,21 @@
 import java.security.*;
-import java.util.Base64;
 
 public class FirmaDigital {
+    // Firma devuelve HEX (en lugar de Base64)
     public static String firmar(byte[] datos, PrivateKey clavePrivada) throws Exception {
         Signature sig = Signature.getInstance("SHA256withRSA");
         sig.initSign(clavePrivada);
         sig.update(datos);
-        return Base64.getEncoder().encodeToString(sig.sign());
+        byte[] firma = sig.sign();
+        return Encriptador.bytesToHex(firma);
     }
 
-    public static boolean verificarFirma(byte[] datos, String firmaB64, PublicKey clavePublica) throws Exception {
+    // Verifica usando firma HEX
+    public static boolean verificarFirma(byte[] datos, String firmaHex, PublicKey clavePublica) throws Exception {
         Signature sig = Signature.getInstance("SHA256withRSA");
         sig.initVerify(clavePublica);
         sig.update(datos);
-        byte[] firma = Base64.getDecoder().decode(firmaB64);
+        byte[] firma = Encriptador.hexToBytes(firmaHex);
         return sig.verify(firma);
     }
 }
